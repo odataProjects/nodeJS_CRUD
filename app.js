519ecaefb744e7d3240bb7dd5877b6ef7eabd361
KelySaina <kelysaina@gmail.com>
2023-11-10 20:12:08 +0300

    test

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.post('/inEmployee', (req, res) => {
 
     conn.query('INSERT INTO Employee (nom_employee, salaire_employee) VALUES (?,?)', [name, salaire], (err, results) => {
         if (err) {
-            console.error('Error executing query:', err);
+            //console.error('Error executing query:', err);
             return res.send('Error inserting employee');
         }
 
@@ -29,7 +29,7 @@ app.post('/inEmployee', (req, res) => {
 app.get('/listEmployees', (req, res) => {
     conn.query('SELECT * FROM Employee', (err, results) => {
         if (err) {
-            console.error('Error executing query:', err);
+            //console.error('Error executing query:', err);
             return;
         }
         res.send(results)
@@ -40,7 +40,7 @@ app.get('/getEmployee/:id', (req, res) => {
     const id = req.params.id;
     conn.query('SELECT * FROM Employee where id_employee = ?', [id], (err, results) => {
         if (err) {
-            console.error('Error executing query:', err);
+            //console.error('Error executing query:', err);
             return;
         }
         res.send(results)
@@ -52,7 +52,7 @@ app.post('/modEmployee', (req, res) => {
 
     conn.query('UPDATE Employee SET nom_employee = ?, salaire_employee = ? WHERE id_employee = ?', [newName, newSalaire, id], (err, results) => {
         if (err) {
-            console.error('Error executing query:', err);
+            //console.error('Error executing query:', err);
             return res.send('Error updating employee');
         }
 
@@ -66,7 +66,7 @@ app.delete('/delEmployee/:id', (req, res) => {
 
     conn.query('DELETE FROM Employee WHERE id_employee = ?', [employeeId], (err, results) => {
         if (err) {
-            console.error('Error executing query:', err);
+            //console.error('Error executing query:', err);
             return res.status(500).send('Error deleting employee');
         }
 
@@ -79,7 +79,7 @@ app.delete('/delEmployee/:id', (req, res) => {
 app.get('/getSalaire', (req, res) => {
     conn.query('SELECT MAX(salaire_employee) as max, MIN(salaire_employee) as min FROM Employee', (err, results) => {
         if (err) {
-            console.error('Error executing query:', err);
+            //console.error('Error executing query:', err);
             return;
         }
         res.send(results)
