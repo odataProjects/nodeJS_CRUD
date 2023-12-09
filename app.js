bff0ba45c5b89c7bd30747905d7aa152bf035e4f
KelySaina <kelysaina@gmail.com>
2023-12-09 12:53:53 +0300

    version 2

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(cors());
 
 app.get("/", (req, res) => {
-    res.send({ msg: "Welcome to nodeJenkins API latest version 2.0" })
+    res.send({ msg: "Welcome to nodeJenkins API latest version 3.0" })
 })
 
 app.post('/inEmployee', (req, res) => {
@@ -76,7 +76,7 @@ app.delete('/delEmployee/:id', (req, res) => {
 
 
 
-/*app.get('/getSalaire', (req, res) => {
+app.get('/getSalaire', (req, res) => {
     conn.query('SELECT MAX(salaire_employee) as max, MIN(salaire_employee) as min FROM Employee', (err, results) => {
         if (err) {
             //console.error('Error executing query:', err);
@@ -84,6 +84,6 @@ app.delete('/delEmployee/:id', (req, res) => {
         }
         res.send(results)
     });
-});*/
+});
 
 module.exports = app;
